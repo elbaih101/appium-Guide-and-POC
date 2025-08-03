84632b9304fe1ccc6618b8a1a5457b3f60cc2c8e
elbaih101 <moustfaelbaih@gmail.com>
2025-08-03 12:43:31 +0300

    added read from cabaplilties file

--- src/main/java/org/example/drivers/DriverManager.java
+++ src/main/java/org/example/drivers/DriverManager.java
@@ -12,6 +12,8 @@ import java.io.File;
 import java.net.MalformedURLException;
 
 import java.net.URL;
+import java.nio.file.Path;
+import java.util.Optional;
 
 
 public class DriverManager {
@@ -30,31 +32,27 @@ public class DriverManager {
     public static void initDriver() throws MalformedURLException {
 
         if (driver == null) {
-            File capbilities=new File("src/main/resources/inspector/capabilities.json");
-            JsonObject caps= JsonUtils.readJson(capbilities.getPath()).getAsJsonObject();
-            UiAutomator2Options options = new UiAutomator2Options()    .setDeviceName("Test")
-
-//                    .setPlatformName("Android")
-
-//                    .setAutomationName("uiautomator2")
-
-//                    .setUdid("emulator-5554")
-//                    .setAppPackage("com.google.android.deskclock")
-//                    .setAppActivity("com.android.deskclock.DeskClock");
-
-                   // .setApp("D:\\Projects\\appium-Guide-and-POC\\src\\main\\resources\\apps\\ApiDemos-debug.apk");
+            File capbilities = new File("src/main/resources/inspector/capabilities.json");
+            JsonObject caps = JsonUtils.readJson(capbilities.getPath()).getAsJsonObject();
+            UiAutomator2Options options = new UiAutomator2Options()
 
 
+                    .setDeviceName(caps.has("appium:deviceName") ? caps.get("appium:deviceName").getAsString():null)
 
-                    .setDeviceName(caps.get("appium:deviceName").getAsString())
-//
                     .setPlatformName(caps.get("platformName").getAsString())
-//
-                    .setAutomationName(caps.get("appium:automationName").getAsString())
-//
-                    .setUdid(caps.get("appium:deviceName").getAsString())
-//
-                    .setApp(caps.get("appium:app").getAsString());
+
+                    .setAutomationName(caps.has("appium:automationName") ? caps.get("appium:automationName").getAsString():null)
+
+                    .setUdid(caps.has("appium:uuid") ? caps.get("appium:uuid").getAsString():null)
+
+                    .setApp(
+                            Optional.ofNullable(caps.has("appium:app") && !caps.get("appium:app").isJsonNull()
+                                            ? caps.get("appium:app").getAsString()
+                                            : null)
+                                    .map(path -> new File(path).getAbsolutePath())
+                                    .orElse(null))
+                    .setAppPackage(caps.has("appium:appPackage") ? caps.get("appium:appPackage").getAsString() : null)
+                    .setAppActivity(caps.has("appium:appActivity") ? caps.get("appium:appActivity").getAsString() : null);
 
 
             URL url = new URL("http://0.0.0.0:4723");
